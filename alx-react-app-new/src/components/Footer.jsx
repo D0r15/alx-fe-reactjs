0acfd23f208cf0bdb9e5970adf13d23b388db5ea
doris <mwangidorisw@gmail.com>
2025-07-21 15:55:04 +0300

    new react app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        
+<footer>
+  <p><span style={{color : 'purple', fontweight : 'bold'}}>© 2023 City Lovers</span></p>
+</footer>
+
+    )
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+
+<main>
+  <p> <span style= {{fontweight : 'bold', color : 'orchid', fontstyle : 'italic'}}>I love to visit New York, Paris, and Tokyo.</span></p>
+</main>
+    )
+}
+export default MainContent;
