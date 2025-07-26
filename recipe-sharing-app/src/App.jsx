a42c219550354fedf7c5d4e9dc074d8fab08cb4c
doris <mwangidorisw@gmail.com>
2025-07-26 11:18:37 +0300

    app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,31 +4,21 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails'; 
 import RecipeStore from './components/recipeStore';
+import RecipeDetail from './components/RecipeDetail';
 import SearchBar from './components/SearchBar';
 
 const App = () => {
-  const setRecipes = useRecipeStore((state) => state.setRecipes);
-
-  useEffect(() => {
-    // Dummy data for example purposes
-    setRecipes([
-      { id: 1, title: 'Chicken Curry', cookingTime: 45, ingredients: ['chicken', 'spices'] },
-      { id: 2, title: 'Beef Stew', cookingTime: 60, ingredients: ['beef', 'onion'] },
-      { id: 3, title: 'Veggie Stir Fry', cookingTime: 20, ingredients: ['vegetarian', 'soy'] },
-    ]);
-  }, []);
-
-  return (
-    <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6">Recipe Finder</h1>
-      <SearchBar/>
-      <RecipeList />
-      <AddRecipeForm/>
-      <RecipeStore/>
-      <RecipeDetails/>
-    
-
-    </div>
+  return ( 
+    <Router>
+      <div className="max-w-4xl mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-6">Recipe Finder</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
